daf3129a980ac74bc2037fa0c656a1de52a2236d
layrton007 <125692365+layrton007@users.noreply.github.com>
2023-03-31 15:25:38 -0300

    Update script.js

--- aluno/LayrtonLindo/web1/javascript/js/script.js
+++ aluno/LayrtonLindo/web1/javascript/js/script.js
@@ -1,5 +1,32 @@
-//lert("olá, mundo!");
+//alert("olá, mundo!");
+//function myHello() {
+    // alert("Olá mundo!");
+    //var lbl = document.getElementById("spnMsg");
 
-function myHello(){
-    alert("Olá mundo!")
+  //  lbl.innerHTML = "olá mundo!";
+//}
+
+function myHello2() {
+    // alert("Olá mundo!");
+
+    var iptMsg = document.getElementById("iptMsg");
+    var msg = iptMsg.value;
+
+
+    var lbl = document.querySelector("#lblMsg");
+    lbl.style.color = "black";
+
+    lbl.textContent = msg;
+}
+
+function apagar() {
+
+    var lbl = document.querySelector("#lblMsg");
+    lbl.textContent = "";
+}
+
+function mudarCor() {
+
+    var lbl = document.querySelector("#lblMsg");
+    lbl.style.color = "red" ;
 }
